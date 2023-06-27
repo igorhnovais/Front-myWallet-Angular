a59aecbb20ec7a01d62b113f10b016f47e30b201
Igor Novais <igorhnovaisdriven@gmail.com>
2023-06-26 21:51:07 -0300

    feat: impport fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormSignInComponent } from './components/form-sign-in/form-sign-in.component';
 import { SignInComponent } from './components/pages/sign-in/sign-in.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,8 @@ import { SignInComponent } from './components/pages/sign-in/sign-in.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
