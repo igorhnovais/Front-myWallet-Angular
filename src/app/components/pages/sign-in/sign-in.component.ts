39750715d7e8be9d6d210b20feeb03b6d5839c00
Igor Novais <igorhnovaisdriven@gmail.com>
2023-06-26 21:52:19 -0300

    feat: router /home

--- src/app/components/pages/sign-in/sign-in.component.ts
+++ src/app/components/pages/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SignIn } from 'src/app/SignIn';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -9,10 +10,15 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class SignInComponent implements OnInit{
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private router: Router
+    ) {}
+    
   ngOnInit(): void {}
 
   async newLogin(signIn: SignIn){
     await this.userService.newLogin(signIn).subscribe();
+    this.router.navigate(["/home"]);
   }
 }
