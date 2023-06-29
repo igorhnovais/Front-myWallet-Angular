2e6c25843b19fc6ea57b816b49ef2a17111adf2e
Igor Novais <igorhnovaisdriven@gmail.com>
2023-06-29 16:55:21 -0300

    feat: route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { SignInComponent } from './components/pages/sign-in/sign-in.component';
+import { NewEntryComponent } from './components/pages/new-entry/new-entry.component';
+import { NewExitComponent } from './components/pages/new-exit/new-exit.component';
+import { SignUpComponent } from './components/pages/sign-up/sign-up.component';
 
 
 const routes: Routes = [
-  {path:"", component: HomeComponent},
-  {path:"sign-in", component: SignInComponent}
+  {path:"sign-in", component: SignInComponent},
+  {path:"sign-up", component: SignUpComponent},
+  {path:"home", component: HomeComponent},
+  {path:"new-entry", component: NewEntryComponent},
+  {path:"new-exit", component: NewExitComponent}
 ];
 
 @NgModule({
