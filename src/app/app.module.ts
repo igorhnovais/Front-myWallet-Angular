c4a56e5f1decf55e4328451730945293c4dee717
Igor Novais <igorhnovaisdriven@gmail.com>
2023-07-24 20:35:28 -0300

    feat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { SignUpComponent } from './components/pages/sign-up/sign-up.component';
 import { MoveComponent } from './components/pages/move/move.component';
 import { EditMoveComponent } from './components/pages/edit-move/edit-move.component';
 import { FormEditComponent } from './components/form-edit/form-edit.component';
+import { FormNewEntryComponent } from './components/form-new-entry/form-new-entry.component';
+import { FormNewExitComponent } from './components/form-new-exit/form-new-exit.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +32,8 @@ import { FormEditComponent } from './components/form-edit/form-edit.component';
     MoveComponent,
     EditMoveComponent,
     FormEditComponent,
+    FormNewEntryComponent,
+    FormNewExitComponent,
   ],
   imports: [
     BrowserModule,
